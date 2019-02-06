b57db83040c5dc2528118a3ef64c1f7486e2f84a
webber2408 <webcrat.tech@gmail.com>
2019-02-06 15:45:30 +0530

    Updates

---
+++ precache-manifest.2f45f1e1dbc8b61c982aaaf426cf219c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc8f01bfbd79eb985d4",
+    "url": "/React-TodoApp/static/js/runtime~main.ccc8f01b.js"
+  },
+  {
+    "revision": "9a52659088751ac47861",
+    "url": "/React-TodoApp/static/js/main.9a526590.chunk.js"
+  },
+  {
+    "revision": "5849ec62572ef070ae9d",
+    "url": "/React-TodoApp/static/js/1.5849ec62.chunk.js"
+  },
+  {
+    "revision": "9a52659088751ac47861",
+    "url": "/React-TodoApp/static/css/main.6ecaf5cd.chunk.css"
+  },
+  {
+    "revision": "143042406180d87f2a68a7735e8b1471",
+    "url": "/React-TodoApp/index.html"
+  }
+];
